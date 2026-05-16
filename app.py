9efba3c365ef8e77ca97fbdc3267a637a3afc585
Nikhil Mohan Tatfale <182417198+nikhiltatfale@users.noreply.github.com>
2026-05-16 19:25:39 +0530

    Add static file support to FastAPI app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 import joblib
 import numpy as np
 import os
 
 app = FastAPI()
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
